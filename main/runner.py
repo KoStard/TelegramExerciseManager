43be4b4c9aa71ef56bc730a25bee57c6eeb6c60a
KoStard <rub099999720@gmail.com>
2019-02-15 22:55:23 +0400

    Listening to python files of repository for changes -> will automatically restartwhen changed.

--- main/runner.py
+++ main/runner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 This is the main runner file of TelegramProblemGenerator.
 Just call run function and it will continuously update all bots from Bot table.
@@ -7,11 +9,32 @@ All logs are collected in the logs.txt.
 
 import sys
 import os
+import platform
 import logging
 import time
 from datetime import datetime
 from threading import Thread
 import django
+
+from os.path import getmtime
+
+
+def get_listening_files(base=None):
+    res = []
+    if not base:
+        base = os.path.abspath(os.path.dirname(__file__)+ '/../..')
+    for fl in os.listdir(base):
+        current = os.path.join(base, fl)
+        if os.path.isfile(current):
+            if current.split('.')[-1] == 'py':
+                res.append(current)
+        else:
+            res += get_listening_files(current)
+    return res
+
+WATCHED_FILES = get_listening_files()
+WATCHED_FILES_MTIMES = [(f, getmtime(f)) for f in WATCHED_FILES]
+
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_PATH)
@@ -21,9 +44,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 django.setup()
 from django.utils import timezone
 from main.worker import *
+from django.core.management import call_command
 
 bots = Bot.objects.all()
 running = True
+file_checks = 0
 
 
 def run():
@@ -58,13 +83,29 @@ def run():
                 return
 
     def update(bot):
+        global running, file_checks
         while running:
+            if file_checks % 20 < 2:
+                if get_listening_files() != WATCHED_FILES_MTIMES:
+                    running = False
+                    print("File's name or count has been changed.")
+                    return
+
+            file_checks += 1
+            for f, mtime in WATCHED_FILES_MTIMES:
+                if getmtime(f) != mtime:
+                    running = False
+                    print("Found changed file!")
+                    return
             try:
-                update_bot(bot)
+                if running:
+                    sleep(0.5)
+                    # update_bot(bot)
             except Exception as e:
                 logging.warning("ERROR: {}".format(e))
                 time.sleep(1)
 
+    autorestart = True
     ts = []
     for bot in bots:
         t = Thread(target=update, args=(bot,))
@@ -73,6 +114,13 @@ def run():
         ts.append(t)
     for t in ts:
         t.join()
+
+    call_command('migrate')
+    if autorestart and platform.system() != 'Windows':
+        # Restarting the script if on macOS or Linux -> has to be executable -> chmod a+x runner.py
+        # Won't match new python files
+        print("Migrating...")
+        os.execv(__file__, sys.argv)
 
 
 if __name__ == '__main__':
