628b57e7f8678df028bfdaf0ed82f759a9c19bf3
KoStard <rub099999720@gmail.com>
2019-08-09 00:05:43 +0400

    Trying to fix bug with images sending.

--- main/command_handlers/answer_problem.py
+++ main/command_handlers/answer_problem.py
@@ -7,27 +7,26 @@ from django.utils import timezone
 
 def answer_problem(worker):
     """ Will send the answer of the problem -> automatically is answering to the active problem """
-    if not worker.source.participant_group.activeProblem and len(worker.source.raw_text.split()) <= 1:
+    if not worker.source.participant_group.activeProblem and len(
+            worker.source.raw_text.split()) <= 1:
         worker.source.bot.send_message(
             worker.source.participant_group,
             "There is no active problem for this participant_group.",
             reply_to_message_id=worker.source.message["message_id"],
         )
         return
-    
+
     # Registering message instance
     if worker.source.command == 'answer':
         MessageInstance.objects.create(
             action_type=ActionType.objects.get(value='problem_command'),
             date=datetime.fromtimestamp(
-                worker['message']["date"],
-                tz=timezone.get_current_timezone()),
+                worker['message']["date"], tz=timezone.get_current_timezone()),
             message_id=worker.source.message['message_id'],
             participant=worker.participant,
             participant_group=worker.participant_group,
             text=worker.source.raw_text,
-            current_problem=worker.participant_group.activeProblem
-        )
+            current_problem=worker.participant_group.activeProblem)
     problem = worker.source.participant_group.activeProblem
     if len(worker.source.raw_text.split()) > 1:
         index = int(worker.source.raw_text.split()[1])
@@ -46,109 +45,117 @@ def answer_problem(worker):
                 worker.source.bot.send_message(worker.source.participant_group,
                                                "Invalid problem number {}.")
             else:
-                resps = worker.source.bot.send_message(worker.source.participant_group, problem.get_answer())
+                resps = worker.source.bot.send_message(
+                    worker.source.participant_group, problem.get_answer())
                 for resp in resps:
                     MessageInstance.objects.create(
-                        action_type=ActionType.objects.get(value='problem_associated'),
+                        action_type=ActionType.objects.get(
+                            value='problem_associated'),
                         date=datetime.fromtimestamp(
-                            resp["date"],
-                            tz=timezone.get_current_timezone()),
+                            resp["date"], tz=timezone.get_current_timezone()),
                         message_id=resp['message_id'],
                         participant=None,
                         participant_group=worker.participant_group,
                         text=None,
-                        current_problem=problem
-                    )
-                for problemimage in sorted(problem.problemimage_set.filter(for_answer=True), key=lambda img: img.id):
+                        current_problem=problem)
+                for problemimage in sorted(
+                        problem.problemimage_set.filter(for_answer=True),
+                        key=lambda img: img.id):
                     image = problemimage.image
                     try:
-                        resps = worker.source.bot.send_image(
+                        resp = worker.source.bot.send_image(
                             worker.source.participant_group,
                             open(image.path, "rb"),
-                            caption="Image of problem N{}'s answer.".format(problem.index),
+                            caption="Image of problem N{}'s answer.".format(
+                                problem.index),
                         )
-                        worker.unilog("Sending image for problem {}'s answer".format(problem.index))
+                        worker.unilog(
+                            "Sending image for problem {}'s answer".format(
+                                problem.index))
                     except Exception as e:
-                        worker.unilog("Can't send image {} for problem N{}'s answer.".format(
-                            image, problem.index))
+                        worker.unilog(
+                            "Can't send image {} for problem N{}'s answer.".
+                            format(image, problem.index))
                         print(e)
                         logging.info(e)
                     else:
-                        for resp in resps:
-                            MessageInstance.objects.create(
-                                action_type=ActionType.objects.get(value='problem_associated'),
-                                date=datetime.fromtimestamp(
-                                    resp["date"],
-                                    tz=timezone.get_current_timezone()),
-                                message_id=resp['message_id'],
-                                participant=None,
-                                participant_group=worker.participant_group,
-                                text=None,
-                                current_problem=problem
-                            )
+                        MessageInstance.objects.create(
+                            action_type=ActionType.objects.get(
+                                value='problem_associated'),
+                            date=datetime.fromtimestamp(
+                                resp["date"],
+                                tz=timezone.get_current_timezone()),
+                            message_id=resp['message_id'],
+                            participant=None,
+                            participant_group=worker.participant_group,
+                            text=None,
+                            current_problem=problem)
             return
-    resps = worker.source.bot.send_message(worker.source.participant_group, problem.get_answer())
+    resps = worker.source.bot.send_message(worker.source.participant_group,
+                                           problem.get_answer())
     for resp in resps:
         MessageInstance.objects.create(
             action_type=ActionType.objects.get(value='problem_associated'),
             date=datetime.fromtimestamp(
-                resp["date"],
-                tz=timezone.get_current_timezone()),
+                resp["date"], tz=timezone.get_current_timezone()),
             message_id=resp['message_id'],
             participant=None,
             participant_group=worker.participant_group,
             text=None,
-            current_problem=problem
-        )
-    
-    for problemimage in sorted(problem.problemimage_set.filter(for_answer=True), key=lambda img: img.id):
+            current_problem=problem)
+
+    for problemimage in sorted(
+            problem.problemimage_set.filter(for_answer=True),
+            key=lambda img: img.id):
         image = problemimage.image
         try:
-            resps = worker.source.bot.send_image(
+            resp = worker.source.bot.send_image(
                 worker.source.participant_group,
                 open(image.path, "rb"),
                 caption="Image of problem N{}'s answer.".format(problem.index),
             )
-            worker.unilog("Sending image for problem {}'s answer".format(problem.index))
+            worker.unilog("Sending image for problem {}'s answer".format(
+                problem.index))
         except Exception as e:
-            worker.unilog("Can't send image {} for problem N{}'s answer.".format(
-                image, problem.index))
+            worker.unilog(
+                "Can't send image {} for problem N{}'s answer.".format(
+                    image, problem.index))
             print(e)
             logging.info(e)
         else:
-            for resp in resps:
-                MessageInstance.objects.create(
-                    action_type=ActionType.objects.get(value='problem_associated'),
-                    date=datetime.fromtimestamp(
-                        resp["date"],
-                        tz=timezone.get_current_timezone()),
-                    message_id=resp['message_id'],
-                    participant=None,
-                    participant_group=worker.participant_group,
-                    text=None,
-                    current_problem=problem
-                )
+            MessageInstance.objects.create(
+                action_type=ActionType.objects.get(value='problem_associated'),
+                date=datetime.fromtimestamp(
+                    resp["date"], tz=timezone.get_current_timezone()),
+                message_id=resp['message_id'],
+                participant=None,
+                participant_group=worker.participant_group,
+                text=None,
+                current_problem=problem)
     old_positions = worker.participant_group.get_participants_positions()
-    resps = worker.source.bot.send_message(worker.source.participant_group, problem.close(worker.source.participant_group))
+    resps = worker.source.bot.send_message(
+        worker.source.participant_group,
+        problem.close(worker.source.participant_group))
     for resp in resps:
         MessageInstance.objects.create(
             action_type=ActionType.objects.get(value='problem_associated'),
             date=datetime.fromtimestamp(
-                resp["date"],
-                tz=timezone.get_current_timezone()),
+                resp["date"], tz=timezone.get_current_timezone()),
             message_id=resp['message_id'],
             participant=None,
             participant_group=worker.participant_group,
             text=None,
-            current_problem=problem
-        )
-    
+            current_problem=problem)
+
     new_positions = worker.participant_group.get_participants_positions()
-    worker.source.position_change = {key: old_positions[key] - new_positions[key] for key in
-                                     new_positions}
+    worker.source.position_change = {
+        key: old_positions[key] - new_positions[key]
+        for key in new_positions
+    }
     t_pages = worker.source.participant_group.telegraphpage_set.all()
     if t_pages:  # Create the page manually with DynamicTelegraphPageCreator
-        t_page = t_pages[len(t_pages) -
+        t_page = t_pages[
+            len(t_pages) -
             1]  # Using last added page -> negative indexing is not supported
         t_account = t_page.account
         page_controller = DynamicTelegraphPageCreator(t_account.access_token)
--- main/command_handlers/send_problem.py
+++ main/command_handlers/send_problem.py
@@ -14,7 +14,7 @@ def send_problem(worker) -> None:
             "You have to close active problem before sending another one.",
             reply_to_message_id=worker.source.message['message_id'])
         return
-    
+
     if len(worker.source.raw_text.split()) > 1:
         index = int(worker.source.raw_text.split()[1])
         try:
@@ -37,7 +37,8 @@ def send_problem(worker) -> None:
     else:
         if not worker.source.participant_group.activeSubjectGroupBinding.last_problem:
             worker.answer_to_the_message(
-                "There was no active problem in this group, so use the command like this - /send 1")
+                "There was no active problem in this group, so use the command like this - /send 1"
+            )
             return
         problem: Problem = worker.source.participant_group.activeSubjectGroupBinding.last_problem.next
         if not problem:
@@ -53,54 +54,50 @@ def send_problem(worker) -> None:
         MessageInstance.objects.create(
             action_type=ActionType.objects.get(value='problem_command'),
             date=datetime.fromtimestamp(
-                worker['message']["date"],
-                tz=timezone.get_current_timezone()),
+                worker['message']["date"], tz=timezone.get_current_timezone()),
             message_id=worker.source.message['message_id'],
             participant=worker.participant,
             participant_group=worker.participant_group,
             text=worker.source.raw_text,
-            current_problem=problem
-        )
-    
-    form_resp = worker.source.bot.send_message(worker.source.participant_group, str(problem))
+            current_problem=problem)
+
+    form_resp = worker.source.bot.send_message(worker.source.participant_group,
+                                               str(problem))
 
     for resp in form_resp:
         MessageInstance.objects.create(
             action_type=ActionType.objects.get(value='problem_associated'),
             date=datetime.fromtimestamp(
-                resp["date"],
-                tz=timezone.get_current_timezone()),
+                resp["date"], tz=timezone.get_current_timezone()),
             message_id=resp['message_id'],
             participant=None,
             participant_group=worker.participant_group,
             text=None,
-            current_problem=problem
-        )
+            current_problem=problem)
 
     logging.debug("Sending problem {}".format(problem.index))
     logging.debug(form_resp)
-    for problemimage in sorted(problem.problemimage_set.filter(for_answer=False), key=lambda img: img.id):
+    for problemimage in sorted(
+            problem.problemimage_set.filter(for_answer=False),
+            key=lambda img: img.id):
         image = problemimage.image
         try:
-            resps = worker.source.bot.send_image(
+            resp = worker.source.bot.send_image(
                 worker.source.participant_group,
                 open(image.path, "rb"),
                 reply_to_message_id=form_resp[0].get(
                     "message_id"),  # Temporarily disabling
                 caption="Image of problem N{}.".format(problem.index),
             )
-            for resp in resps:
-                MessageInstance.objects.create(
-                    action_type=ActionType.objects.get(value='problem_associated'),
-                    date=datetime.fromtimestamp(
-                        resp["date"],
-                        tz=timezone.get_current_timezone()),
-                    message_id=resp['message_id'],
-                    participant=None,
-                    participant_group=worker.participant_group,
-                    text=None,
-                    current_problem=problem
-                )
+            MessageInstance.objects.create(
+                action_type=ActionType.objects.get(value='problem_associated'),
+                date=datetime.fromtimestamp(
+                    resp["date"], tz=timezone.get_current_timezone()),
+                message_id=resp['message_id'],
+                participant=None,
+                participant_group=worker.participant_group,
+                text=None,
+                current_problem=problem)
             logging.debug("Sending image for problem {}".format(problem.index))
             worker.adm_log("Sent image {} for problem N{}".format(
                 path.basename(image.name), problem.index))
@@ -108,11 +105,11 @@ def send_problem(worker) -> None:
             print("Can't send image {}".format(image))
             print(e)
             logging.info(e)
-            worker.adm_log(
-                "Can't send image {} for problem N{}".format(
-                    image, problem.index))
+            worker.adm_log("Can't send image {} for problem N{}".format(
+                image, problem.index))
     worker.source.participant_group.activeProblem = problem
     worker.source.participant_group.save()
     worker.source.participant_group.activeSubjectGroupBinding.last_problem = problem
     worker.source.participant_group.activeSubjectGroupBinding.save()
-    register_current_participant_group_members_count(worker)  # Registering participants count with send command
+    register_current_participant_group_members_count(
+        worker)  # Registering participants count with send command
